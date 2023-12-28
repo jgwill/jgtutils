043858eaeaac0c32f35e06c392fc35b5b553019a
JGWill Gaia.WSL <jgi@jgwill.com>
2023-12-27 22:20:44 -0500

    script

---
+++ release-n-test.sh
@@ -0,0 +1,7 @@
+
+. scripts/version-patcher.sh
+cversion=$(cat setup.py |tr '"' " " |awk '/version/ {print $3}')
+git commit . -m "v$cversion" && git tag "$cversion" && git push --tags && git push 
+
+make dist && twine upload dist/* && sleep 32 &&  (conda activate jgtpy310 && pip install -U jgtutils && conda activate jgtfxcon && pip install -U jgtutils)
+
