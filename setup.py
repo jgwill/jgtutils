6e1d6f6688c6123387aaac252e3628d2e97575bb
JGWill Gaia.WSL <jgi@jgwill.com>
2023-12-28 21:35:05 -0500

    v0.1.10

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jgtutils',
-    version='0.1.9',
+    version='0.1.10',
     packages=find_packages(),
     description='A utility package common to other JGT projects.',
     long_description=open('README.md').read(),
