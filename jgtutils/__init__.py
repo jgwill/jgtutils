9c51282ef2ad513aa3fce03f0249fa59d82c40bb
JGWill <jgi@jgwill.com>
2024-02-09 18:06:54 -0500

    v0.1.20

--- jgtutils/__init__.py
+++ jgtutils/__init__.py
@@ -2,7 +2,7 @@
 jgtutils package
 """
 
-__version__ = "0.1.19"
+__version__ = "0.1.20"
 
 
 from jgtutils import jgtpov as pov
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ INSTALL_REQUIRES = [
 ]
 setup(
     name='jgtutils',
-    version='0.1.19',
+    version='0.1.20',
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
     description='A utility package common to other JGT projects.',
