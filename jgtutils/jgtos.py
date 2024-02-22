5de5b9768d11292f04c10d4c35579d1e33cdf15c
JGWill <jgi@jgwill.com>
2024-02-22 15:53:28 -0500

    fix

--- jgtutils/jgtos.py
+++ jgtutils/jgtos.py
@@ -13,11 +13,12 @@ def create_filestore_path(
     compressed=False, 
     tlid_range:str=None,
     output_path:str=None,
-    nsdir:str="pds"
+    nsdir:str="pds",
+    read_full=False
 ):
     # Define the file path based on the environment variable or local path
     if output_path is None:
-        data_path = get_data_path(nsdir=nsdir)
+        data_path = get_data_path(nsdir=nsdir,read_full=read_full)
     else: # get path from var in os
         data_path = os.path.join (output_path.replace("/"+nsdir,""),nsdir)
     
--- release-n-test.sh
+++ release-n-test.sh
@@ -3,5 +3,5 @@
 cversion=$(cat setup.py |tr "'" " " |awk '/version/ {print $2}')
 git commit . -m "v$cversion" && git tag "$cversion" && git push --tags && git push 
 
-make dist && twine upload dist/* && sleep 32 &&  (conda activate jgtpy310 && pip install -U jgtutils && conda activate jgtfxcon && pip install -U jgtutils)
+make dist && twine upload dist/* && sleep 32 &&  (conda activate jgtpy310 && pip install -U jgtutils ;(conda activate jgtfxcon && pip install -U jgtutils) )
 
