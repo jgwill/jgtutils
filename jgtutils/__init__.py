51b143685d6d70e84f28aaab0c897c705347e4fb
JGWill <jgi@jgwill.com>
2024-04-14 04:49:16 -0400

    v0.1.51

--- jgtutils/__init__.py
+++ jgtutils/__init__.py
@@ -2,7 +2,7 @@
 jgtutils package
 """
 
-__version__ = "0.1.50"
+__version__ = "0.1.51"
 
 
 from jgtutils import jgtpov as pov
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ INSTALL_REQUIRES = [
 ]
 setup(
     name='jgtutils',
-    version='0.1.50',
+    version='0.1.51',
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
     description='A utility package common to other JGT projects.',
