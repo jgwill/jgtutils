a4d5fc2bae4f75fa290aa94b599314145837ca5c
JGWill <jgi@jgwill.com>
2024-05-06 09:25:10 -0400

    v0.1.61

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ INSTALL_REQUIRES = [
 ]
 setup(
     name='jgtutils',
-    version='0.1.60',
+    version='0.1.61',
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
     description='A utility package common to other JGT projects.',
