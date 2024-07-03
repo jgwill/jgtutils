7e0001662d4a15b12d7172a3cb2056524674ff9b
JGWill <jgi@jgwill.com>
2024-07-03 10:40:25 -0400

    chg:packaging and post-dist

--- release-n-test.sh
+++ release-n-test.sh
@@ -1,9 +1,15 @@
+. $HOME/.bashrc&> /dev/null
+. .env||echo "No .env file found.  Assuming that is fine"
 
 . scripts/version-patcher.sh
-cversion=$(cat setup.py |tr "'" " " |awk '/version/ {print $2}')
+cversion=$(cat pyproject.toml |tr '"' " " |awk '/version/ {print $3}')
 git commit . -m "v$cversion" && git tag "$cversion" && git push --tags && git push 
 
 make dist && twine upload dist/* && sleep 32 &&  (conda activate jgtpy310 && pip install -U jgtutils ;(conda activate jgtfxcon && pip install -U jgtutils) )
 
 # install upgrade in the user environment
-((sleep 25;conda activate base;pip install --user -U jgtutils) &> /dev/null)&
+#if [ "$CONDA_ENV_PROD_BASE" == "" ]; then
+#	CONDA_ENV_PROD_BASE=$(conda info --base||echo "baseprod")
+#fi
+#((sleep 25;conda activate $CONDA_ENV_PROD_BASE &&  pip install --user -U jgtutils) &> /dev/null)&
+sleep 15;postdistpyproject
--- scripts/version-patcher.sh
+++ scripts/version-patcher.sh
@@ -11,7 +11,7 @@ npm version patch && \
 if [ "$cversion" == "" ]; then echo "bahhhh cversion has no value"
 else
 	echo "Patching files for $cversion from $pversion"
-	for f in setup.py jgtutils/__init__.py ;do sed -i 's/'$pversion'/'$cversion'/g' $f;done
+	for f in setup.py jgtutils/__init__.py pyproject.toml;do sed -i 's/'$pversion'/'$cversion'/g' $f;done
 fi
 else echo "Must executer $0 from current directory of package.json"
 fi
