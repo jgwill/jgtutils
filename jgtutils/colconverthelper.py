f3f7db908712dcceb29ff5a9e997cae04e667e5a
JGWill <jgi@jgwill.com>
2024-07-12 00:43:47 -0400

    add:return type

--- jgtutils/colconverthelper.py
+++ jgtutils/colconverthelper.py
@@ -3,7 +3,7 @@ from jgtconstants import MFI_SQUAT, MFI_FAKE, MFI_FADE, MFI_GREEN, MFI_SQUAT_STR
 from jgtutils.jgtconstants import MFI_VAL as MFI_DEFAULT_COLNAME
 from jgtutils.jgtconstants import ZCOL as ZONE_DEFAULT_COLNAME
 
-def mfi_str_to_id(mfi_str:str):
+def mfi_str_to_id(mfi_str:str)->int:
     if mfi_str == MFI_SQUAT_STR:
         return MFI_SQUAT_ID
     elif mfi_str == MFI_FAKE_STR:
@@ -15,7 +15,7 @@ def mfi_str_to_id(mfi_str:str):
     else:
         return 0
 
-def mfi_signal_to_str(mfi_signal:int):
+def mfi_signal_to_str(mfi_signal:int)->str:
     if mfi_signal == MFI_SQUAT:
         return MFI_SQUAT_STR
     elif mfi_signal == MFI_FAKE:
@@ -61,7 +61,7 @@ def get_mfi_features_column_list_by_timeframe(t:str,mfi_colname=""):
 
 from jgtconstants import ZONE_INT,ZONE_BUY_STR,ZONE_BUY_ID,ZONE_SELL_STR,ZONE_SELL_ID,ZONE_NEUTRAL_STR,ZONE_NEUTRAL_ID
 
-def zone_str_to_id(zone_str:str):
+def zone_str_to_id(zone_str:str)->int:
     if zone_str == ZONE_BUY_STR:
         return ZONE_BUY_ID
     elif zone_str == ZONE_SELL_STR:
@@ -71,7 +71,7 @@ def zone_str_to_id(zone_str:str):
     else:
         return ZONE_NEUTRAL_ID
 
-def zone_id_to_str(zone_id:int):
+def zone_id_to_str(zone_id:int)->str:
     if zone_id == ZONE_BUY_ID:
         return ZONE_BUY_STR
     elif zone_id == ZONE_SELL_ID:
