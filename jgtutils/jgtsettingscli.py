726b318076fffc161e4887fb6da96d41a1434c18
JGWill <jgi@jgwill.com>
2024-08-14 16:46:20 -0400

    cleanup:jgtset cli

--- jgtutils/jgtsettingscli.py
+++ jgtutils/jgtsettingscli.py
@@ -9,18 +9,12 @@ import jgtcommon
 def parse_args():
     parser = jgtcommon.new_parser("JGTFxCLI Settings Loader", "Load and output settings as JSON", enable_specified_settings=True)
     parser=jgtcommon.add_settings_argument(parser)
-    # parser = jgtcommon.add_instrument_timeframe_arguments(parser)
-    # parser = jgtcommon.add_bars_amount_V2_arguments(parser)
-    # parser = jgtcommon.add_keepbidask_argument(parser)
-    # parser = jgtcommon.add_tlid_range_argument(parser)
-    # parser = jgtcommon.add_verbose_argument(parser)
     args = jgtcommon.parse_args(parser)
     return args
 
 def main():
     args = parse_args()
-    settings = jgtcommon.load_settings()
-    print("Settings loaded:")
+    settings = jgtcommon.get_settings()
     print(json.dumps(settings, indent=4))
 
 if __name__ == '__main__':
