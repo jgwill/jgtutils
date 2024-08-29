44908a9ab8c49c15bcddfd8d47571f50c9f41749
JGWill <jgi@jgwill.com>
2024-08-29 09:31:26 -0400

    chg:Enhanced for short and state.

--- jgtutils/jgtclihelper.py
+++ jgtutils/jgtclihelper.py
@@ -1,9 +1,17 @@
 import json
-def print_jsonl_message(msg,extra_dict:dict=None,scope=None):
+def print_jsonl_message(msg,extra_dict:dict=None,scope=None,state:str=None,msg_key_name = "message",state_key_name = "state",scope_key = "scope",use_short=False):
     o={}
-    o["message"]=msg
+    if use_short:
+        msg_key_name="msg"
+        state_key_name="s"
+        scope_key="sc"
+    o[msg_key_name]=msg
     if extra_dict:
         o.update(extra_dict)
     if scope:
-        o["scope"]=scope
+        
+        o[scope_key]=scope
+    if state:
+        
+        o[state_key_name]=state
     print(json.dumps(o))
