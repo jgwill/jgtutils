6e216aeab6ce02b21405ab150cc4dd3999140e03
JGWill <jgi@jgwill.com>
2024-08-30 10:54:54 -0400

    chg:tfw app enhanced and prep
    SILENT and EXIT when TF Reached
    * -t m1 -X -N
    
    First proto
    issue #4

--- jgtutils/cli_test_cronrun_helper.py
+++ jgtutils/cli_test_cronrun_helper.py
@@ -4,9 +4,11 @@ import os
 
 import sys
 
+
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 import jgtcommon
+from jgtclihelper import print_jsonl_message
 
 import jgtwslhelper as wsl
 
@@ -22,14 +24,29 @@ def refreshTF( timeframe:str,quote_count:int=-1, quiet:bool=True,use_full:bool=F
     print("Error in refreshPH")
     raise e
 
+timeframe=None
+from jgtclihelper import build_jsonl_message
+def _exit_quietly_handler():
+  global timeframe
+  msg = f"Wait canceled by user"
+  o=build_jsonl_message(msg,extra_dict={"timeframe":timeframe},state="canceled",scope=APP_SCOPE,use_short=True)
+  return o
+
 def parse_args():
-    parser = jgtcommon.new_parser("JGT Cron Test","Test launching or unlocking (exit when specific timeframes arrives.)",enable_specified_settings=True)
+    parser = jgtcommon.new_parser("JGT Cron Test","Test launching or unlocking (exit when specific timeframes arrives.)",enable_specified_settings=True,exiting_quietly_handler=_exit_quietly_handler)#add_exiting_quietly_flag=True,exiting_quietly_message=f"")
     #parser=jgtcommon.add_settings_argument(parser)
     #parser=jgtcommon._preload_settings_from_args(parser)
     
     parser= jgtcommon.add_timeframe_standalone_argument(parser,required=True)
     #exit the program when the timeframe is reached
     parser.add_argument("-X", "--exit", action="store_true", help="Exit the program when the timeframe is reached.")
+    #--message
+    parser.add_argument("-M", "--message", help="Message to display when the timeframe is reached.",default="Timeframe reached.")
+    
+    parser.add_argument("-I", "--in-message", help="Message to display when the timeframe wait starts.",default="Timeframe waiting started")
+    
+    #--nooutput
+    parser.add_argument("-N", "--no-output", action="store_true", help="Do not output anything.")
     #parser=jgtcommon.add_bars_amount_V2_arguments(parser)
     #parser=jgtcommon.add_keepbidask_argument(parser)
     #parser=jgtcommon.add_tlid_range_argument(parser)
@@ -37,31 +54,45 @@ def parse_args():
     
     args = jgtcommon.parse_args(parser)
     return args
-
+APP_SCOPE="tfwait"
 def main():
-    args = parse_args()
-    ctx_times = get_times_by_timeframe_str(args.timeframe)
-    print(f"CTX times: {ctx_times}")
-    sleep_duration = 60 if args.timeframe != "m1" else 2
+    global timeframe
+    #add_exiting_quietly()
     
+    args = parse_args()
+    timeframe = args.timeframe
+    
+    ctx_times = get_times_by_timeframe_str(timeframe)
+    quiet = args.quiet
+    if not quiet:
+      print(f"CTX times: {ctx_times}")
+    sleep_duration = 60 if timeframe != "m1" else 2
+    
+
+    if not args.no_output:
+      _print_app_message(timeframe,args.in_message,state="started" )
     while True:
-      current_time = datetime.datetime.now().strftime("%H:%M") if args.timeframe != "m1" else datetime.datetime.now().strftime("%H:%M:%S")
-      # H4 = ["01:00", "05:00", "9:00", "13:00","17:00","21:00"]
-      # H1=["00:00","01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"]
-      # m15=["00:00","00:15","00:30","00:45","01:00", "01:15", "01:30", "01:45", "02:00", "02:15", "02:30", "02:45", "03:00", "03:15", "03:30", "03:45", "04:00", "04:15", "04:30", "04:45", "05:00", "05:15", "05:30", "05:45", "06:00", "06:15", "06:30", "06:45", "07:00", "07:15", "07:30", "07:45", "08:00", "08:15", "08:30", "08:45", "09:00", "09:15", "09:30", "09:45", "10:00", "10:15", "10:30", "10:45", "11:00", "11:15", "11:30", "11:45", "12:00", "12:15", "12:30", "12:45", "13:00", "13:15", "13:30", "13:45", "14:00", "14:15", "14:30", "14:45", "15:00", "15:15", "15:30", "15:45", "16:00", "16:15", "16:30", "16:45", "17:00", "17:15", "17:30", "17:45", "18:00", "18:15", "18:30", "18:45", "19:00", "19:15", "19:30", "19:45", "20:00", "20:15", "20:30", "20:45", "21:00", "21:15", "21:30", "21:45", "22:00", "22:15", "22:30", "22:45", "23:00", "23:15", "23:30", "23:45"]
-      # m15b=get_timeframes_times_by_minutes(15)
-      # m5=get_timeframes_times_by_minutes(5)
+      current_time = get_current_time(timeframe)
+     
       
       if current_time in ctx_times:  # Adjust the times as needed
           if args.exit:
-              print("Exiting to do what is next")
+              #print(args.message," ",args.timeframe," reached at ",current_time)
+              if not args.no_output:
+                _print_app_message(timeframe, args.message,state="reached")
+              #sys.stdout.flush()
               exit(0)
           output = refreshTF(args.timeframe)
           print(output)
           time.sleep(sleep_duration)  # Sleep for 60 seconds to avoid multiple runs within the same minute
       time.sleep(1)  # Check every second
       #print(".", end="")
-    sys.stdout.flush()
+
+def _print_app_message(timeframe, msg,state=None,use_short=True):
+    print_jsonl_message(msg,extra_dict={"timeframe":timeframe,"time":get_current_time(timeframe)},scope=APP_SCOPE,state=state,use_short=use_short)
+
+def get_current_time(timeframe):
+    return datetime.datetime.now().strftime("%H:%M") if timeframe != "m1" else datetime.datetime.now().strftime("%H:%M:%S")
 
 def get_times_by_timeframe_str(timeframe:str):
   if timeframe=="D1" or timeframe=="W1" or timeframe=="M1":
@@ -109,6 +140,8 @@ def get_timeframe_daily_ending_time() -> str:
         return "22:00:00"
     else:
         return "21:00:00"
+
+
 
 if __name__ == '__main__':
     main()
